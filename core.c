8df592b78dc3222eac26af10de225f2762b0fb9b
Mickael TACNET <mtacnet@e1r11p23.42.fr>
2017-07-10 13:26:17 +0200

    minishell

--- core.c
+++ core.c
@@ -6,13 +6,30 @@
 /*   By: mtacnet <mtacnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/10 12:34:51 by mtacnet           #+#    #+#             */
-/*   Updated: 2017/07/10 12:51:20 by mtacnet          ###   ########.fr       */
+/*   Updated: 2017/07/10 13:25:42 by mtacnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+ ** if_path: Prend en parametre un path d'acces valide (usr/bin/cd par exemple)
+ ** Prend en second arg un tableau contenant en pos[0] la commande et en pos[1]
+ ** l'option ou le path au quel on souhaite accéder. Pour finir, le 3ieme arg
+ ** contient la copie de l'environnement actuel.
+*/
+
 void	if_path(char *valid_path, char **tab_arg, char **env)
 {
+	pid_t		child_pid;
+	
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		
+	}
+	else
+	{
 
+	}
 }
